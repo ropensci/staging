a4da5573477a89f58d7aaa49e8aea35116cc2f0d
Jeroen Ooms <jeroenooms@gmail.com>
2019-01-05 14:39:30 +0100

    Install the R package

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#set -e
 export TERM="${TERM:-xterm}"
 source "${BASH_SOURCE%/*}/ci-library.sh"
 
@@ -13,12 +14,12 @@ message "Building package(s): ${packages}"
 for package in "${packages[@]}"; do
     orig_url=$(head -1 "packages/${package}")
     orig_name=$(basename $orig_url)
-    rm -Rf "build-$package" && mkdir "build-$package" && cd "build-$package"
+    rm -Rf "src" && mkdir "src" && cd "src"
     curl -LSsk $orig_url -o $orig_name
     mkdir -p upstream
     tar xf $orig_name -C upstream --strip-components=1
     R CMD build upstream --no-build-vignettes --no-manual
     rm -Rf upstream $orig_name
-    Rscript --vanilla -e "tools::write_PACKAGES(verbose = TRUE)"
+    #Rscript --vanilla ../scripts/install.R *.tar.gz
     cd -
 done
---
+++ scripts/install.R
@@ -0,0 +1,23 @@
+install_with_deps <- function(pkg){
+	cat("Installing: ", pkg, "\n")
+	stopifnot(file.exists(pkg))
+	path <- normalizePath(dirname(pkg), winslash = "/")
+	name <- strsplit(basename(pkg), "_")[[1]][1]
+	# Install current version + depds from binary packages
+	try(install.packages(name, dependencies = TRUE))
+
+	# Install dev version
+	tools::write_PACKAGES(path, type = 'source', verbose = TRUE)
+	if(is_windows()) unlink(file.path(path, 'PACKAGES.gz'))
+	contrib_url <- c(paste0("file:", path), contrib.url(getOption('repos'), 'source'))
+	install.packages(name, contriburl = contrib_url, type = 'source', dependencies = TRUE)
+}
+
+is_windows <- function(){
+	.Platform$OS.type == 'windows'
+}
+
+Sys.setenv(TAR = "internal")
+options(repos = c(CRAN = "https://cloud.r-project.org"))
+setRepositories(ind = 1:4)
+install_with_deps(commandArgs(TRUE))
